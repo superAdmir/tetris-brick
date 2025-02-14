029edb3216bda19fb14d96ca762a78200db5e929
Super Admir Dev <superadmirdev@gmail.com>
2025-02-14 13:35:58 +0100

    New commit

--- app/src/main/java/com/tb/tetrisbrick/game/ui/settings/SettingsActivity.java
+++ app/src/main/java/com/tb/tetrisbrick/game/ui/settings/SettingsActivity.java
@@ -92,7 +92,7 @@ public class SettingsActivity extends AppCompatActivity implements SettingsView 
     //Method is used to load the rewarded ads
     private void loadRewardedAd() {
         AdRequest adRequest = new AdRequest.Builder().build();
-        RewardedAd.load(this, "ca-app-pub-3940256099942544/5224354917", adRequest, new RewardedAdLoadCallback() {
+        RewardedAd.load(this, "ca-app-pub-6402675413704299/3493711545", adRequest, new RewardedAdLoadCallback() {
             @Override
             public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
                 // Handle the error.
